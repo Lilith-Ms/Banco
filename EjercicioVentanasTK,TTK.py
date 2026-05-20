62e954ce2befe38b160963d85c28747ca0c8dc76
Lilith-Ms <jaz.chik.283@gmail.com>
2026-05-19 21:04:28 -0500

    Feat: Ventanas

---
+++ EjercicioVentanasTK,TTK.py
@@ -0,0 +1,41 @@
+from tkinter import Tk, ttk, messagebox
+
+def saludar():
+    nombre = entrada.get()
+
+    if nombre != "":
+        messagebox.showinfo("Saludo", f"Hola {nombre}, bienvenid@")
+    else:
+        messagebox.showwarning("Advertencia", "Escriba un nombre")
+
+def limpiar():
+    entrada.delete(0, "end")
+    entrada.focus()
+
+
+root = Tk()
+root.title("Saludo")
+root.geometry("400x200")
+
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+lbl = ttk.Label(frm, text="Ingrese su nombre")
+lbl.grid(column=3, row=3, padx=10, pady=10)
+
+entrada = ttk.Entry(frm, width=30)
+entrada.grid(column=0, row=1, columnspan=3, padx=5, pady=5)
+
+entrada.focus()
+
+
+btn_saludar = ttk.Button(frm, text="Saludar", command=saludar)
+btn_saludar.grid(column=0, row=2, padx=5, pady=10)
+
+btn_limpiar = ttk.Button(frm, text="Limpiar", command=limpiar)
+btn_limpiar.grid(column=1, row=2, padx=5, pady=10)
+
+btn_salir = ttk.Button(frm, text="Salir", command=root.destroy)
+btn_salir.grid(column=2, row=2, padx=5, pady=10)
+
+root.mainloop()
